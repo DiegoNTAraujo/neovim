668d4fa13de7bef62e68e0c72251858e6d28e79a
DiegoNTAraujo <diegonascimentoteixeiraaraujo@gmail.com>
2026-02-27 17:30:18 -0300

    feat(lsp): add nvim-lspconfig, mason, and mason-tool-installer

--- init.lua
+++ init.lua
@@ -5,4 +5,6 @@ require("plugins.colorscheme")
 require("plugins.explorer")
 require("plugins.statusline")
 
+require("plugins.lsp")
+
 require("plugins.telescope")
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+vim.pack.add({
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" }
+})
+
+require("mason").setup()
+require("mason-tool-installer").setup({
+  ensure_installed = {
+    "lua-language-server"
+  }
+})
+
+vim.lsp.enable({
+  "lua_ls"
+})
+
+vim.diagnostic.config {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵"
+    }
+  },
+  float = {
+    header = false,
+    border = "rounded",
+  }
+}
+
+vim.keymap.set("n", "<leader>sd", ":lua vim.diagnostic.open_float()<CR>", {})
+vim.keymap.set("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", {})
